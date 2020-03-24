e67eac7c5606998064ba006108ba318da31d38fc
ttcom <1902305073@qq.com>
2020-03-24 22:33:29 +0800

    addsomthing

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import './common/css/reset.scss'
 import './common/css/index.scss'
 import './permission'
+//import 'lib-flexible/flexible'
+
 import { 
   breadcrumbItem,Button, Select,Message,breadcrumb,table,tableColumn,pagination,Row,Input,
   Col,Card,Drawer,Dialog,MessageBox,Form,Loading,
